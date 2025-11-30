aa2c161310e0a00d680a4935990eaa962d710303
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-12-01 00:47:15 +0800

    refactor(build-support/mk-wallpaper-package): add packaging helper

---
+++ pkgs/build-support/mk-wallpaper-package/default.nix
@@ -0,0 +1,71 @@
+{
+  lib,
+  stdenvNoCC,
+}:
+
+{
+  displayName ? null,
+  ...
+}@args:
+
+stdenvNoCC.mkDerivation (
+  finalAttrs:
+  (
+    let
+      pname = finalAttrs.pname;
+      displayName = args.displayName or pname;
+      licenseSpdxId = finalAttrs.meta.license.spdxId;
+    in
+    {
+      installPhase = ''
+        runHook preInstall
+
+        local bg_dir=$out/share/backgrounds/${pname}
+        local gnome_xml_dir=$out/share/gnome-background-properties
+        local kde_wp_dir=$out/share/wallpapers/${pname}
+        local xml_file=$gnome_xml_dir/${pname}.xml
+
+        # Do installation
+        mkdir -p $bg_dir
+        mkdir -p $gnome_xml_dir
+        mkdir -p $kde_wp_dir/contents
+        cp -r * $bg_dir/
+        ln -s $bg_dir $kde_wp_dir/contents/images
+
+        # Write GNOME metadata
+        touch $xml_file
+        echo >> $xml_file '<?xml version="1.0" encoding="UTF-8"?>'
+        echo >> $xml_file '<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">'
+        echo >> $xml_file '<wallpapers>'
+
+        for img in "$bg_dir"/*; do
+          if [ -f "$img" ]; then
+            filename=$(basename "$img")
+            name="''${filename%.*}"
+
+            echo >> $xml_file '  <wallpaper deleted="false">'
+            echo >> $xml_file '    <name>'"$name"'</name>'
+            echo >> $xml_file '    <filename>'"$img"'</filename>'
+            echo >> $xml_file '    <options>zoom</options>'
+            echo >> $xml_file '    <pcolor>#000000</pcolor>'
+            echo >> $xml_file '    <scolor>#000000</scolor>'
+            echo >> $xml_file '    <shade_type>solid</shade_type>'
+            echo >> $xml_file '  </wallpaper>'
+          fi
+        done
+
+        echo >> $xml_file '</wallpapers>'
+
+        # Write KDE metadata
+        cp ${./metadata.template.desktop} $kde_wp_dir/metadata.desktop
+        substituteInPlace $kde_wp_dir/metadata.desktop \
+          --replace-fail "@@package-name@@" "${pname}" \
+          --replace-fail "@@display-name@@" "${displayName}" \
+          --replace-fail "@@license-spdx-id@@" "${licenseSpdxId}" \
+
+        runHook postInstall
+      '';
+    }
+    // (builtins.removeAttrs args [ "displayName" ])
+  )
+)
