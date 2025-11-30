ea10191fdb806ed8b58899c5bcb4a0f345c06892
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-11-30 23:00:43 +0800

    init: initialize repository

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "StarryNix-Resources";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts/main";
+    };
+  };
+
+  outputs =
+    { self, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { self', pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              nil
+              nixfmt-rfc-style
+            ];
+          };
+
+          formatter = pkgs.nixfmt-tree;
+
+          legacyPackages = { };
+
+          packages = inputs.nixpkgs.lib.filterAttrs (
+            _: value: inputs.nixpkgs.lib.isDerivation value
+          ) self'.legacyPackages;
+        };
+    };
+}
