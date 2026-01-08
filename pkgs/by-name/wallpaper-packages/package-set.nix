24f5f18777c07872060da36c707f1849b528cbf3
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-01-08 19:17:31 +0800

    fix(wallpaper-packages): doesn't include new packages

--- pkgs/by-name/wallpaper-packages/package-set.nix
+++ pkgs/by-name/wallpaper-packages/package-set.nix
@@ -1,5 +1,9 @@
 callPackage: {
+  celestial = callPackage ./celestial/package.nix { };
+
   digital-art = callPackage ./digital-art/package.nix { };
 
   landscape-illustration = callPackage ./landscape-illustration/package.nix { };
+
+  minimalism = callPackage ./minimalism/package.nix { };
 }
